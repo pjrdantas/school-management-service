3efc47770934ae2840b23f94c21de298147c56ef
Paulo Dantas <20131563+pjrdantas@users.noreply.github.com>
2026-04-23 23:49:04 -0300

    feat(student): implementa endpoint de cadastro de aluno

---
+++ src/main/java/br/com/escola/schoolmanagement/studentmanagement/application/usecase/CriarAlunoUseCase.java
@@ -0,0 +1,40 @@
+package br.com.escola.schoolmanagement.studentmanagement.application.usecase;
+
+import br.com.escola.schoolmanagement.studentmanagement.adapter.in.web.AlunoRequest;
+import br.com.escola.schoolmanagement.studentmanagement.adapter.in.web.AlunoResponse;
+import br.com.escola.schoolmanagement.studentmanagement.adapter.out.persistence.entity.AlunoEntity;
+import br.com.escola.schoolmanagement.studentmanagement.adapter.out.persistence.repository.AlunoJpaRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CriarAlunoUseCase {
+
+    private final AlunoJpaRepository alunoJpaRepository;
+
+    public CriarAlunoUseCase(AlunoJpaRepository alunoJpaRepository) {
+        this.alunoJpaRepository = alunoJpaRepository;
+    }
+
+    public AlunoResponse executar(AlunoRequest request) {
+        if (alunoJpaRepository.findByCpf(request.cpf()).isPresent()) {
+            throw new IllegalArgumentException("Já existe aluno cadastrado com este CPF");
+        }
+
+        AlunoEntity alunoEntity = new AlunoEntity();
+        alunoEntity.setNomeCompleto(request.nomeCompleto());
+        alunoEntity.setCpf(request.cpf());
+        alunoEntity.setEmail(request.email());
+        alunoEntity.setDataNascimento(request.dataNascimento());
+
+        AlunoEntity persisted = alunoJpaRepository.save(alunoEntity);
+
+        return new AlunoResponse(
+                persisted.getId(),
+                persisted.getNomeCompleto(),
+                persisted.getCpf(),
+                persisted.getEmail(),
+                persisted.getDataNascimento(),
+                persisted.getCreatedAt()
+        );
+    }
+}
